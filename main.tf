95c65f38dddf8c909a3ce86ef546b2d6f0ab0de7
Ramu Pasupuleti <126960469+ramupasupuleti011@users.noreply.github.com>
2026-05-25 20:03:26 +0530

    Change key_name from 'ramu' to 'hello' for instances

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ region = "us-east-1"
 resource "aws_instance" "one" {
   ami             = "ami-0236922087fa98b6e"
   instance_type   = "t2.micro"
-  key_name        = "ramu"
+  key_name        = "hello"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "us-east-1a"
   user_data       = <<EOF
@@ -25,7 +25,7 @@ EOF
 resource "aws_instance" "two" {
   ami             = "ami-0236922087fa98b6e"
   instance_type   = "t2.micro"
-  key_name        = "ramu"
+  key_name        = "hello"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "us-east-1b"
   user_data       = <<EOF
@@ -44,7 +44,7 @@ EOF
 resource "aws_instance" "three" {
   ami             = "ami-0236922087fa98b6e"
   instance_type   = "t2.micro"
-  key_name        = "ramu"
+  key_name        = "hello"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "us-east-1a"
   tags = {
@@ -55,7 +55,7 @@ resource "aws_instance" "three" {
 resource "aws_instance" "four" {
   ami             = "ami-0236922087fa98b6e"
   instance_type   = "t2.micro"
-  key_name        = "ramu"
+  key_name        = "hello"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "us-east-1b"
   tags = {
